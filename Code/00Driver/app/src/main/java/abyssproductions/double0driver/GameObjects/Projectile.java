3155a738e3c88923697d2e01f3b29b52aa23e166
Mandip Sangha <mandeeip@hotmail.com>
2017-02-01 20:05:52 -0800

    Created the projectile class and a skeleton for the class

---
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Projectile.java
@@ -0,0 +1,32 @@
+package abyssproductions.double0driver.GameObjects;
+
+/**
+ * Created by Mandip Sangha on 2/1/2017.
+ */
+
+public class Projectile extends GameObject {
+    /*  PURPOSE:    Constructor for the projectile that sets the default values for the object
+        INPUT:      NONE
+        OUTPUT:     NONE
+     */
+    public Projectile(){
+
+    }
+
+    /*  PURPOSE:    Updates the projectile's logic
+        INPUT:      NONE
+        OUTPUT:     NONE
+     */
+    public void update(){
+
+    }
+
+    /*  PURPOSE:    Launches the projectile from the X and Y position given 
+        INPUT:      x                   - The X position to launch the projectile from
+                    y                   - The Y position to launch the projectile from
+        OUTPUT:     NONE
+     */
+    public void launched(float x, float y){
+
+    }
+}
