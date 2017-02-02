bc0174b3a15889cd9c708cb061f32a1fa25405b9
Mandip Sangha <mandeeip@hotmail.com>
2017-02-02 15:16:48 -0800

    set some default values and implement the move, dimension and draw methods and started on creating new variables need for animation

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/GameObject.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/GameObject.java
@@ -2,27 +2,37 @@ package abyssproductions.double0driver.GameObjects;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.RectF;
 
 /**
  * Created by Mandip Sangha on 1/31/2017.
  */
 
 public class GameObject {
-    //  PURPOSE:    Holds the object's current location and size
-    private Rect myDimensions;
-    //  PURPOSE:    Holds the object's image
+    //  PURPOSE:    Holds the object's left, top, right, bottom coordinates
+    private RectF myDimensions;
+
+    private Paint myPaint;
+    //  PURPOSE:    Holds the object's current frame image location
+    private Rect myCurFrameLoc;
+    //  PURPOSE:    Holds the object's images
     protected Bitmap myImage;
     //  PURPOSE:    Holds the object's movement velocity
     protected Point myVelocity;
+
+    protected AnimateState myCurAniState;
+    public enum AnimateState{Normal,Destroyed}
 
     /*  PURPOSE:    Constructor for the Game Object that set the default value for the object
         INPUT:      NONE
         OUTPUT:     NONE
      */
     public GameObject(){
-
+        myDimensions.set(0,0,0,0);
+        myVelocity.set(0,0);
     }
 
     /*  PURPOSE:    Draws the game object's image to the screen
@@ -30,7 +40,7 @@ public class GameObject {
         OUTPUT:     NONE
     */
     public void draw(Canvas canvas){
-
+        canvas.drawBitmap(myImage,myCurFrameLoc,myDimensions,myPaint);
     }
 
     /*  PURPOSE:    Updates the game object's logic
@@ -53,15 +63,15 @@ public class GameObject {
         INPUT:      newDimension    - The new position and size to set
         OUTPUT:     NONE
     */
-    public void setMyDimensions(Rect newDimension){
-
+    public void setMyDimensions(RectF newDimension){
+        myDimensions.set(newDimension);
     }
 
-    /*  PURPOSE:    Return's the game object's current location and size
+    /*  PURPOSE:    Return's the game object's left, top, right, bottom coordinates
         INPUT:      NONE
         OUTPUT:     A Rect object with the data
     */
-    public Rect getDimensions(){
+    public RectF getDimensions(){
         return myDimensions;
     }
 
@@ -69,15 +79,15 @@ public class GameObject {
         INPUT:      moveBy          - The amount to move the game object by
         OUTPUT:     NONE
     */
-    protected void moveVertical(int moveBy){
-
+    protected void moveVertical(float moveBy){
+        myDimensions.offset(0,moveBy);
     }
 
     /*  PURPOSE:    Move's the game object horizontally by the amount given
         INPUT:      moveBy          - The amount to move the game object by
         OUTPUT:     NONE
     */
-    protected void moveHorizontal(int moveBy){
-
+    protected void moveHorizontal(float moveBy){
+        myDimensions.offset(moveBy,0);
     }
 }
