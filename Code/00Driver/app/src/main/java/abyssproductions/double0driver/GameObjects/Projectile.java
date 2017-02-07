8905f747e8e83a0fa09b3beb62e4194608b72bc3
Mandip Sangha <mandeeip@hotmail.com>
2017-02-07 14:17:27 -0800

    implement launched method and the logic for it in the update method

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Projectile.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Projectile.java
@@ -1,5 +1,9 @@
 package abyssproductions.double0driver.GameObjects;
 
+import android.graphics.RectF;
+
+import abyssproductions.double0driver.GameGlobals;
+
 /**
  * Created by Mandip Sangha on 2/1/2017.
  */
@@ -18,15 +22,23 @@ public class Projectile extends GameObject {
         OUTPUT:     NONE
      */
     public void update(){
-
+        RectF tempDim = getDimensions();
+        moveVertical(myVelocity.y);
+        if(tempDim.bottom < 0 || tempDim.top > 555/*NEED TO CHANGE VAR*/)myVelocity.set(0,0);
     }
 
     /*  PURPOSE:    Launches the projectile from the X and Y position given 
         INPUT:      x                   - The X position to launch the projectile from
                     y                   - The Y position to launch the projectile from
+                    direction           - The direction the projectile will travel (0 for down, 1
+                                            for up)
         OUTPUT:     NONE
      */
-    public void launched(float x, float y){
-
+    public void launched(float x, float y,int direction){
+        RectF temp = getDimensions();
+        temp.offsetTo(x,y);
+        setMyDimensions(temp);
+        if(direction == 0)myVelocity.set(0,1);
+        else myVelocity.set(0,-1);
     }
 }
