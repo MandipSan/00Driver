005c6cff6f2de24e603f55a2cd854e9b60cb9b01
Mandip Sangha <mandeeip@hotmail.com>
2017-02-10 10:20:43 -0800

    merged with master

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Items.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Items.java
@@ -1,5 +1,8 @@
 package abyssproductions.double0driver.GameObjects;
 
+import android.graphics.Rect;
+import android.graphics.RectF;
+
 /**
  * Created by Mandip Sangha on 2/1/2017.
  * Edited by Mark Reffel on 2/9/2017
@@ -9,14 +12,18 @@ public class Items extends GameObject {
     //  PURPOSE:    Holds the item’s type
     private ItemTypes myType;
     //  PURPOSE:    Holds the different type of items
-    public enum ItemTypes{HealthBox}
+    public enum ItemTypes{HealthBox, AmmoBox, MysteryBox}
 
     /*  PURPOSE:    Constructor for the items that sets the default values for the object
-        INPUT:      NONE
+        INPUT:      imageReference      - Reference's the image to be load
+                    imageWidth          - The width of a single image in the image sheet
+                    imageHeight         - The height of a single image in the image sheet
         OUTPUT:     NONE
      */
     public Items(int imageReference, int width, int height){
         super(imageReference, width, height);
+        myVelocity.set(0,1);
+        myType = ItemTypes.HealthBox;
     }
 
     /*  PURPOSE:    Updates the item's logic
@@ -24,7 +31,8 @@ public class Items extends GameObject {
         OUTPUT:     NONE
      */
     public void update(){
-
+        super.update();
+        moveVertical(myVelocity.y);
     }
 
     /*  PURPOSE:    Spawn the item of the type given at the location given
@@ -34,7 +42,10 @@ public class Items extends GameObject {
         OUTPUT:     NONE
      */
     public void spawn(ItemTypes itemType, float x, float y){
-
+        myType = itemType;
+        RectF temp = new RectF(getDimensions());
+        temp.offset(x,y);
+        setMyDimensions(temp);
     }
 
     /*  PURPOSE:    Returns the type of item the item is
