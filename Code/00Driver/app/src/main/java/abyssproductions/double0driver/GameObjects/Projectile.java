af3e0671cefc3ffb4a784d9595bd20d407c75916
mjreffel <mjreffel@gmail.com>
2017-02-10 12:24:31 -0800

    Projectile Merge into master (#16)
    * implement launched method and the logic for it in the update method
    
    * add and implement method to check if projectile is active
    
    * add a test picture to test with
    
    * add test image and size to test with
    
    * modified update method to call the parents update method and increased the projectiles velocity speed for testing
    
    * Tested and add projectiles to game global singleton in an array
    
    * Added in Flamethrower projectile class and updated the projectile class
    
    * Added in basic machine gun projectile class
    
    * added in to the launch function so that it adds to ghe global projectiles array

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Projectile.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Projectile.java
@@ -1,6 +1,7 @@
 package abyssproductions.double0driver.GameObjects;
 
-
+import android.graphics.RectF;
+import abyssproductions.double0driver.R;
 
 /**
  * Created by Mandip Sangha on 2/1/2017.
@@ -16,20 +17,40 @@ public class Projectile extends GameObject {
         super(imageReference, width, height);
     }
 
+    public Projectile() {
+        this(R.drawable.test, 50, 50);
+    }
+
     /*  PURPOSE:    Updates the projectile's logic
         INPUT:      NONE
         OUTPUT:     NONE
      */
     public void update(){
-
+        super.update();
+        RectF tempDim = getDimensions();
+        moveVertical(myVelocity.y);
+        if(tempDim.bottom < 0 || tempDim.top > 555/*NEED TO CHANGE VAR*/)myVelocity.set(0,0);
     }
 
     /*  PURPOSE:    Launches the projectile from the X and Y position given 
         INPUT:      x                   - The X position to launch the projectile from
                     y                   - The Y position to launch the projectile from
+                    direction           - The direction the projectile will travel (-1 for down, 1
+                                            for up)
         OUTPUT:     NONE
      */
-    public void launched(float x, float y){
+    public void launch(float x, float y,int direction){
+        RectF temp = getDimensions();
+        temp.offsetTo(x,y);
+        setMyDimensions(temp);
+        myVelocity.set(0,direction*5);
+    }
 
+    /*  PURPOSE:    Returns if the projectile is active
+        INPUT:      NONE
+        OUTPUT:     Returns boolean of whether active if yes true; if not false
+     */
+    public boolean getActive(){
+        return !myVelocity.equals(0,0);
     }
 }
---
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/ProjectileObjects/MachineGunProjectile.java
@@ -0,0 +1,59 @@
+package abyssproductions.double0driver.GameObjects.ProjectileObjects;
+
+import android.graphics.RectF;
+
+import abyssproductions.double0driver.GameGlobals;
+import abyssproductions.double0driver.GameObjects.Projectile;
+import abyssproductions.double0driver.R;
+
+/**
+ * Created by Mark Reffel on 2/8/17.
+ * Edited by Mark Reffel on 2/9/2017
+ */
+
+public class MachineGunProjectile extends Projectile {
+
+
+    /*  PURPOSE:    Constructor for the Flame Thrower Projectile is to set the default values for the object
+        INPUT:      imageReference      - The image reference for the projectile
+                    width               - The width of the projectile
+                    height              - The height of the projectile
+        OUTPUT:     NONE
+        */
+    private MachineGunProjectile(int ImageReference, int width, int height) {
+        super(ImageReference, width, height);
+    }
+
+    /*  PURPOSE:    Default constructor creates default projector using above constructor
+        INPUT:      NONE
+        OUTPUT:     NONE
+     */
+    public MachineGunProjectile() {
+        this(R.drawable.test, 50, 50);
+    }
+
+
+
+    /*  PURPOSE:    Launches the projectile from the X and Y position given
+    INPUT:      x                   - The X position to launch the projectile from
+                y                   - The Y position to launch the projectile from
+                direction           - The direction the projectile will travel (-1 for down, 1
+                                        for up)
+    OUTPUT:     NONE
+    */
+    public void launch(float x, float y, int direction) {
+        RectF temp = getDimensions();
+        temp.offsetTo(x, y);
+        setMyDimensions(temp);
+        myVelocity.set(0,direction*5);
+        //Find first empty spot in projectiles array
+        for(int i = 0; i < GameGlobals.getInstance().myProjectiles.length; i++) {
+            if(GameGlobals.getInstance().myProjectiles[i] == null) {
+                GameGlobals.getInstance().myProjectiles[i] = this;
+                break;
+            }
+        }
+
+    }
+
+}
