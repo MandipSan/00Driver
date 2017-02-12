7f9ea87bc87ab3afba1705ac3d1b864b595bedd5
Mandip Sangha <mandeeip@hotmail.com>
2017-02-11 20:17:08 -0800

    add helicopter's movement

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Enemy.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameObjects/Enemy.java
@@ -1,6 +1,9 @@
 package abyssproductions.double0driver.GameObjects;
 
 import android.graphics.RectF;
+import android.util.Log;
+
+import java.util.Random;
 
 import abyssproductions.double0driver.GameGlobals;
 import abyssproductions.double0driver.GameObjects.ProjectileObjects.MachineGunProjectile;
@@ -11,10 +14,18 @@ import abyssproductions.double0driver.GameObjects.ProjectileObjects.MachineGunPr
  */
 
 public class Enemy extends Sprite {
+
+    private final static int changeMovementMax = 15;
+    //  PURPOSE:    Timer before helicopter changes it movement
+    private int changeMovement;
+    //  PURPOSE:    Used to help randomize helicopters movement
+    private Random random;
     //  PURPOSE:    Holds the enemy's type
     private EnemyType myType;
     //  PURPOSE:    Holds the different type of enemies
     public enum EnemyType{BasicCar, MachineGunCar, DronePickup, SpikeVan, Helicopter}
+
+
 
     /*  PURPOSE:    Constructor for the basic enemy that sets the default values for the object
         INPUT:      imageReference      - Reference's the image to be load
@@ -24,6 +35,8 @@ public class Enemy extends Sprite {
      */
     public Enemy(int imageReference, int width, int height){
         super(imageReference, width, height);
+        changeMovement = 0;
+        random = new Random();
     }
 
     /*  PURPOSE:    Updates the basic enemy's logic
@@ -32,12 +45,8 @@ public class Enemy extends Sprite {
      */
     public void update(){
         super.update();
-        if(myType != EnemyType.Helicopter){
-            moveVertical(myVelocity.y);
-        }else{
-            moveHorizontal(myVelocity.x);
-            moveVertical(myVelocity.y);
-        }
+        move();
+
     }
 
     /*  PURPOSE:    Spawn the enemy of the type given at the location given
@@ -51,16 +60,18 @@ public class Enemy extends Sprite {
     public void spawn(EnemyType enemyType, float x, float y){
         myType = enemyType;
         RectF temp = getDimensions();
-        temp.offsetTo(x,y);
-        setMyDimensions(temp);
         if(enemyType != EnemyType.Helicopter) {
+            temp.offsetTo(x,y);
+            setMyDimensions(temp);
             if(y < (GameGlobals.getInstance().getScreenHeight()/2)){
                 myVelocity.set(0,-1*GameGlobals.enemiesUniVelocity);
             }else{
                 myVelocity.set(0,GameGlobals.enemiesUniVelocity);
             }
         }else {
-            //TODO:HELICOPTER SPAWNING STILL TO BE DESIGNED
+            temp.offsetTo(x,0-temp.bottom);
+            setMyDimensions(temp);
+            myVelocity.set(0,GameGlobals.enemiesUniVelocity);
         }
     }
 
@@ -88,5 +99,81 @@ public class Enemy extends Sprite {
      */
     public EnemyType getMyType(){
         return myType;
+    }
+
+    /*  PURPOSE:    Enemy's movement logic
+        INPUT:      NONE
+        OUTPUT:     NONE
+     */
+    private void move(){
+        boolean heliReady = false;
+        if(myType != EnemyType.Helicopter){
+            moveVertical(myVelocity.y);
+        }else{
+            RectF temp = getDimensions();
+            //Stops the helicopter from making random movement until the helicopter is full on the
+            // game screen
+            if(temp.top >= 0){
+                heliReady = true;
+            }
+            //Randomly picks the helicopter next movement pattern
+            if(changeMovement <= 0 && heliReady){
+                int rand = random.nextInt(8);
+                switch (rand){
+                    //Stop
+                    case 0:
+                        myVelocity.set(0,0);
+                        break;
+                    //Move Right
+                    case 1:
+                        myVelocity.set(GameGlobals.enemiesUniVelocity, 0);
+                        break;
+                    //Move Left
+                    case 2:
+                        myVelocity.set(-GameGlobals.enemiesUniVelocity, 0);
+                        break;
+                    //Move Up
+                    case 3:
+                        myVelocity.set(0, -GameGlobals.enemiesUniVelocity);
+                        break;
+                    //Move Down
+                    case 4:
+                        myVelocity.set(0, GameGlobals.enemiesUniVelocity);
+                        break;
+                    //Move Up Right
+                    case 5:
+                        myVelocity.set(GameGlobals.enemiesUniVelocity,-GameGlobals.enemiesUniVelocity);
+                        break;
+                    //Move Up Left
+                    case 6:
+                        myVelocity.set(-GameGlobals.enemiesUniVelocity,-GameGlobals.enemiesUniVelocity);
+                        break;
+                    //Move Down Right
+                    case 7:
+                        myVelocity.set(GameGlobals.enemiesUniVelocity,GameGlobals.enemiesUniVelocity);
+                        break;
+                    //Move Down Left
+                    case 8:
+                        myVelocity.set(-GameGlobals.enemiesUniVelocity,GameGlobals.enemiesUniVelocity);
+                        break;
+                }
+                changeMovement = changeMovementMax;
+            }
+            changeMovement--;
+            //Checks to make sure helicopter isn't going to run from the screen anywhere
+            if(temp.right+myVelocity.x > GameGlobals.getInstance().getScreenWidth() ||
+                    temp.left+myVelocity.x < 0){
+                int vel = myVelocity.y;
+                myVelocity.set(0,vel);
+            }
+            if((temp.bottom+myVelocity.y > GameGlobals.getInstance().getScreenWidth() ||
+                    temp.top+myVelocity.y < 0) && heliReady){
+                int vel = myVelocity.x;
+                myVelocity.set(vel,0);
+            }
+
+            moveHorizontal(myVelocity.x);
+            moveVertical(myVelocity.y);
+        }
     }
 }
