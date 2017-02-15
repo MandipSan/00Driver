03d52eef32456b22a4ee38b5d6e3803138b13c55
Mandip Sangha <mandeeip@hotmail.com>
2017-02-14 18:48:09 -0800

    changed to set gamesurfaceview

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/MainActivity.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/MainActivity.java
@@ -3,11 +3,13 @@ package abyssproductions.double0driver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import abyssproductions.double0driver.GameEngine.GameSurfaceView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new GameSurfaceView(this));//R.layout.activity_main);
     }
 }
