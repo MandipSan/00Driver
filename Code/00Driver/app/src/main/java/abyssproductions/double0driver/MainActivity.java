e4079a49b771db8a45ba0aa1e63246e5d0c52914
Mandip Sangha <mandeeip@hotmail.com>
2017-03-27 19:18:43 -0700

    add the pointers to the different fragments

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/MainActivity.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/MainActivity.java
@@ -32,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startScreen = StartScreen.newInstance();
+        gameScreen = GameScreen.newInstance();
+        creditScreen = CreditScreen.newInstance();
+        helpScreen = HelpScreen.newInstance();
+        settingScreen = SettingScreen.newInstance();
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
