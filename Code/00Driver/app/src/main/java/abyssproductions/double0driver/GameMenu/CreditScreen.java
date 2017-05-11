3e337c0b49c6982095efea0df91be6a4eed77128
Mark <mjreffel@gmail.com>
2017-05-11 15:20:53 -0700

    Adding in new game music licenced for commercial use as well as started working on list view and adaptor code for the credits menu

--- Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameMenu/CreditScreen.java
+++ Code/00Driver/app/src/main/java/abyssproductions/double0driver/GameMenu/CreditScreen.java
@@ -1,5 +1,6 @@
 package abyssproductions.double0driver.GameMenu;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,7 +8,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import abyssproductions.double0driver.MainActivity;
 import abyssproductions.double0driver.R;
@@ -17,9 +24,13 @@ import abyssproductions.double0driver.R;
  */
 
 public class CreditScreen extends Fragment {
+
+
     public static CreditScreen newInstance(){
         return new CreditScreen();
     }
+    private ListView creditsView ;
+    private ArrayAdapter<String> creditsAdapter ;
 
     @Override
     public void onAttach(Context context){
@@ -29,6 +40,29 @@ public class CreditScreen extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        // Find the ListView resource.
+        creditsView = (ListView) getActivity().findViewById( R.id.creditsListView );
+
+        // Create and populate a List of planet names.
+        String[] creditRows = new String[] {
+                "Game Engine : Mandip Sangha",
+                "Game Objects : Mandip Sangha",
+                "Projectile Objects : Mark Reffel",
+                "Game Menus : Mark Reffel",
+                "Credits Page : Mark Reffel",
+                "Music By : Drive by Alex (c) copyright 2013 Licensed under a Creative Commons Attribution (3.0) license. http://dig.ccmixter.org/files/AlexBeroza/43098 Ft: cdk & Darryl J"
+        };
+        ArrayList<String> creditsList = new ArrayList<String>();
+        creditsList.addAll( Arrays.asList(creditRows));
+
+
+        // Create ArrayAdapter using the planet list.
+        creditsAdapter = new ArrayAdapter<String>(getContext(), R.layout.credits_row, creditsList);
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        creditsView.setAdapter( creditsAdapter );
         return inflater.inflate(R.layout.credit_screen,container,false);
     }
 
